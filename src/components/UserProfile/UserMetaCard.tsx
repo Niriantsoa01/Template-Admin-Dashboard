b85564a41b2995fe5627385d104fad7a03c4d307
Niriantsoa <123114759+Niriantsoa01@users.noreply.github.com>
2026-01-09 15:49:54 +0300

    Update user location and Facebook link in UserMetaCard

--- src/components/UserProfile/UserMetaCard.tsx
+++ src/components/UserProfile/UserMetaCard.tsx
@@ -28,13 +28,13 @@ export default function UserMetaCard() {
                 </p>
                 <div className="hidden h-3.5 w-px bg-gray-300 dark:bg-gray-700 xl:block"></div>
                 <p className="text-sm text-gray-500 dark:text-gray-400">
-                  Arizona, United States
+                  Madagascar, Tananarive 101
                 </p>
               </div>
             </div>
             <div className="flex items-center order-2 gap-2 grow xl:order-3 xl:justify-end">
               <a
-                href="https://www.facebook.com/PimjoHQ"
+                href="#"
                 target="_blank"
                 rel="noopener"
                 className="flex h-11 w-11 items-center justify-center gap-2 rounded-full border border-gray-300 bg-white text-sm font-medium text-gray-700 shadow-theme-xs hover:bg-gray-50 hover:text-gray-800 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-white/[0.03] dark:hover:text-gray-200"
